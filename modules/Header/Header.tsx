3e6e220e2979de73f02c35ee319fcb84ed52d5cb
David <david.kushnir.d@gmail.com>
2023-04-03 00:33:49 +0300

    fix

--- modules/Header/Header.tsx
+++ modules/Header/Header.tsx
@@ -4,12 +4,19 @@ import s from './Header.module.scss'
 import Nav from './Nav'
 import cn from 'classnames'
 import { useWindowScroll } from '@/modules/Header/useWindowScroll'
+import { useRouter } from 'next/router'
 
 const Header: FC = () => {
+	const { pathname } = useRouter()
 	const [isStatic, animated] = useWindowScroll()
 
 	return (
 		<header
+			style={
+				pathname.startsWith('/manage')
+					? { position: 'relative' }
+					: undefined
+			}
 			className={cn(s.headerWrapper, {
 				[s.static]: isStatic,
 				[s.fixed]: !isStatic,
